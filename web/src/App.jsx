6f0031697ebcf3c023db710bb5bd12f094e53c45
Spencer, Paul Whiting <pwablito@gmail.com>
2023-01-22 11:06:13 -0500

    Improve wording

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,7 +5,7 @@ import SubmissionForm from "./SubmissionForm";
 const App = React.forwardRef((_, __) => {
   return (
     <div className="App">
-      <h1>Welcome to the HackerMatch web client</h1>
+      <h1>Welcome to HackerMatch!</h1>
       <p>Please enter your hash and a contact email here.</p>
       <p>We will send you an email if/when someone else submits the same hash as you.</p>
       <p>If someone has already submitted your hash, you should expect an email from us soon!</p>
